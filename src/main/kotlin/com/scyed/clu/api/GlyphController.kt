9d4e0de9d6dd09b28cd3a6faffc7813c26348a7e
Jackinatox <jakob.schulze1206@gmail.com>
2026-05-03 23:57:10 +0200

    Get Glyph by Id

--- src/main/kotlin/com/scyed/clu/api/GlyphController.kt
+++ src/main/kotlin/com/scyed/clu/api/GlyphController.kt
@@ -4,9 +4,13 @@ import com.scyed.clu.glyph.GlyphEntity
 import com.scyed.clu.glyph.GlyphRepository
 import com.scyed.clu.glyph.GlyphSummary
 import com.scyed.clu.glyph.toSummary
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 
 @RestController
@@ -15,6 +19,15 @@ class GlyphController(
     val glyphRepository: GlyphRepository
 ) {
     @GetMapping
-    fun getAllGlyphs(): List<GlyphSummary> =
-        glyphRepository.findAll().map(GlyphEntity::toSummary)
+    fun getAllGlyphs(): List<GlyphSummary> = glyphRepository.findAll().map(GlyphEntity::toSummary)
+
+
+    @GetMapping("{glyphId}")
+    fun getById(@PathVariable glyphId: Long): GlyphEntity {
+        val glyph = glyphRepository.findById(glyphId)
+        if (glyph.isPresent) {
+            return glyph.get()
+        }
+        throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
 }
